cb1fb4439fa50881f1f8154ce17887bedb9316dc
destroierdam <dam-bb@hotmail.com>
2018-10-16 17:02:24 +0300

    Edit solutions

--- Lesson2/solutions/rook.cpp
+++ Lesson2/solutions/rook.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 int main()
 {
-    int a,b,c,d;
-    cin>>a>>b>>c>>d;
+	// В rx и ry ще пишем реда и колоната на топа,
+	// а в a и b ще пишем реда и колоната на полето, което ни интересува
+    int rx,ry,a,b;
+    cin>>rx>>ry>>a>>b;
 	
 	// Ако сме на същия ред или същата колона
-    if(a==c||b==d)
-        cout<<"YES"<<endl;
+    if(rx==a||ry==b)
+	{
+		cout<<"YES"<<endl;
+	}
     else
+	{
         cout<<"NO"<<endl;
+	}
     return 0;
 }
